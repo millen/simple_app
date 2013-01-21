8ddb1037866024a61492db5f3b52cb8e8c3a29d4
Milen Atanasov <millen.atanasov@gmail.com>
2013-01-21 14:31:39 +0200

    New user fixed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,7 @@ class UsersController < ApplicationController
   end
   
   def create
+    @user = User.new(params[:user])
     if @user.save
 	  sign_in @user
 	  flash[:success] = "Welcome to the Sample App!"
@@ -26,7 +27,6 @@ class UsersController < ApplicationController
   end
   
   def edit
-    @user = User.find(params[:id])
   end
   
   def update
